9072f3249e9a2845b834695490d82b4e73f4511a
Keith Ecker <KeithJohn1020@gmail.com>
2019-02-20 15:46:22 -0600

    Added login and signup ui

---
+++ UW-RateEm/Controller/LoginMenuViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginMenuViewController.swift
+//  UW-RateEm
+//
+//  Created by Keith Ecker on 2/20/19.
+//  Copyright © 2019 Keith Ecker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class LoginMenuViewController: UIViewController{
+    
+}
